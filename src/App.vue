92c5994a88333f7f3d5077e0ae1b200018833f33
Jose Claudio <jcmm.21@hotmail.com>
2024-03-03 13:47:12 -0600

    Se creó el proyecto en Vue e inicio de la interfaz

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header class="logo">
+
+  </header>
+
+  <aside>
+    <nav>
+      <ul>
+        <li>
+          <RouterLink to="/">Home</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/about">About</RouterLink>
+        </li>
+      </ul>
+    </nav>
+  </aside>
+  
+  <RouterView />
+</template>
+
+<style scoped>
+.logo{
+  width: 100%;
+  height: 15%;
+  background-color: gray;
+}
+
+/* Aside */
+aside{
+  display: flex;
+  width: 15%;
+  background-color: #ccc;
+  height: 80%;
+}
+
+</style>
