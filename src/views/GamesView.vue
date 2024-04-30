f60f0d02c9e8d0d4f99be43cc270cfe322b2dcfd
Jose Claudio <jcmm.21@hotmail.com>
2024-04-30 14:05:01 -0600

    Corrección de error Search - El botón de borrar búsqueda no borraba correctamente el valor del input

--- src/views/GamesView.vue
+++ src/views/GamesView.vue
@@ -9,14 +9,14 @@
         </nav>
 
         <!-- Search -->
-        <form class="form">
+        <form class="form" onsubmit = "return false">
             <button>
                 <svg width="17" height="16" fill="none" xmlns="http://www.w3.org/2000/svg" role="img" aria-labelledby="search">
                     <path d="M7.667 12.667A5.333 5.333 0 107.667 2a5.333 5.333 0 000 10.667zM14.334 14l-2.9-2.9" stroke="currentColor" stroke-width="1.333" stroke-linecap="round" stroke-linejoin="round"></path>
                 </svg>
             </button>
-            <input class="input" placeholder="Type your text" required="" type="text" v-model="search" @input="console.log(search)">
-            <button class="reset" type="reset">
+            <input class="input" placeholder="Nombre del juego" type="text" v-model="search">
+            <button class="reset" @click="search = ''">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path>
                 </svg>
